5f9e31fe31df7e64483d7c04c9929d4a61bff3fb
Kayra Uylar <k.uylar@outlook.com>
2023-03-12 22:31:35 +0300

    Add changelog URLs

--- CurseRinth/Controllers/GeneralController.cs
+++ CurseRinth/Controllers/GeneralController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net;
+using System.Web;
+using CurseForge.APIClient;
+using CurseForge.APIClient.Models;
 using CurseRinth.Models;
+using Html2Markdown;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,12 +12,21 @@ namespace CurseRinth.Controllers;
 [ApiController]
 public class GeneralController : Controller
 {
+	private ApiClient Api;
+	private SlugMapper Slug;
+
+	public GeneralController(ApiClient api, SlugMapper slug)
+	{
+		Api = api;
+		Slug = slug;
+	}
+
 	[Route("/")]
 	[HttpGet]
-	public ModrinthApiInfo Index() => 
+	public ModrinthApiInfo Index() =>
 		new("The bridge that no one asked for", "https://docs.modrinth.com", "curserinth", "1.0.0");
-	
-	
+
+
 	[Route("/error")]
 	[HttpGet]
 	public async Task ErrorHandler()
@@ -22,5 +35,24 @@ public class GeneralController : Controller
 		Exception e = exceptionHandlerFeature.Error;
 		Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 		await Response.WriteAsJsonAsync(new ModrinthError(e));
+	}
+
+
+	[Route("/changelog/{projectId}/{fileId}")]
+	[HttpGet]
+	public async Task<ContentResult> Changelog(uint projectId, uint fileId)
+	{
+		GenericResponse<string> modFileChangelogAsync = await Api.GetModFileChangelogAsync(projectId, fileId);
+		try
+		{
+			string changelog = new Converter().Convert(modFileChangelogAsync.Data).Replace("\r\n", "\n");
+			changelog = Utils.LinkoutRegex.Replace(changelog,
+				match => $"({HttpUtility.UrlDecode(HttpUtility.UrlDecode(match.Groups[1].ToString()))})");
+			return Content(changelog, "text/markdown");
+		}
+		catch
+		{
+			return Content(modFileChangelogAsync.Data, "text/html");
+		}
 	}
 }
